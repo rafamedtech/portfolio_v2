e12f0971ae93eeb5ed26abba663361893058b0ac
Rafael <rafa.ravg@gmail.com>
2022-07-01 20:08:23 -0700

    dynamic routes test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 import { defineNuxtConfig } from 'nuxt';
 
+const routes = await queryContent('/blog').find();
+let dynamicRoutes = () => {
+  return new Promise((resolve) => {
+    resolve(routes.map((el) => `blog/${el.slug}`));
+  });
+};
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['@/assets/css/main.css'],
@@ -9,6 +16,17 @@ export default defineNuxtConfig({
   content: {
     highlight: {
       theme: 'dracula',
+    },
+  },
+
+  // generate dynamic routes nuxt 3 content v2
+  generate: {
+    routes: [dynamicRoutes],
+  },
+
+  nitro: {
+    prerender: {
+      routes: ['/sitemap.xml'],
     },
   },
 
