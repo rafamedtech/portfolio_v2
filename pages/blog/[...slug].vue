85454faebfcb469d1c51dd82c28c52127c86297e
Rafael <rafa.ravg@gmail.com>
2022-07-02 08:52:10 -0700

    w/o image test

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -16,9 +16,9 @@ useHead({
   <main
     class="hero-pattern container prose prose-pink max-w-none py-24 px-5 pt-48 prose-headings:text-secondary prose-h3:text-primary prose-p:text-lg prose-p:text-light prose-blockquote:bg-custom prose-blockquote:p-1 prose-strong:text-accent prose-code:text-light md:pt-24"
   >
-    <figure>
+    <!-- <figure>
       <img :src="data.img" alt="" class="clay-effect mx-auto h-1/2 w-full rounded-2xl lg:w-1/2" />
-    </figure>
+    </figure> -->
     <!-- <ContentDoc class="container" /> -->
     <ContentRenderer class="container" :value="data" />
   </main>
