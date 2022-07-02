f0ac0aa6a5cfa95623a2bc17e0e33347c88c6975
Rafael <rafa.ravg@gmail.com>
2022-07-02 09:15:15 -0700

    suspense test

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,14 +1,7 @@
 <script setup>
 const { path } = useRoute();
-// const { data } = await useAsyncData(`blog/${path}`, () => {
-//   return queryContent().where({ _path: path }).findOne();
-// });
-
-onMounted(async () => {
-  const { data } = await useAsyncData(`blog/${path}`, () => {
-    return queryContent().where({ _path: path }).findOne();
-  });
-  return data;
+const { data } = await useAsyncData(`blog/${path}`, () => {
+  return queryContent().where({ _path: path }).findOne();
 });
 
 useHead({
@@ -20,13 +13,15 @@ useHead({
 </script>
 
 <template>
-  <main
-    class="hero-pattern container prose prose-pink max-w-none py-24 px-5 pt-48 prose-headings:text-secondary prose-h3:text-primary prose-p:text-lg prose-p:text-light prose-blockquote:bg-custom prose-blockquote:p-1 prose-strong:text-accent prose-code:text-light md:pt-24"
-  >
-    <figure>
-      <img :src="data.img" alt="" class="clay-effect mx-auto h-1/2 w-full rounded-2xl lg:w-1/2" />
-    </figure>
-    <ContentDoc class="container" />
-    <!-- <ContentRenderer class="container" :value="data" /> -->
-  </main>
+  <Suspense>
+    <main
+      class="hero-pattern container prose prose-pink max-w-none py-24 px-5 pt-48 prose-headings:text-secondary prose-h3:text-primary prose-p:text-lg prose-p:text-light prose-blockquote:bg-custom prose-blockquote:p-1 prose-strong:text-accent prose-code:text-light md:pt-24"
+    >
+      <figure>
+        <img :src="data.img" alt="" class="clay-effect mx-auto h-1/2 w-full rounded-2xl lg:w-1/2" />
+      </figure>
+      <ContentDoc class="container" />
+      <!-- <ContentRenderer class="container" :value="data" /> -->
+    </main>
+  </Suspense>
 </template>
