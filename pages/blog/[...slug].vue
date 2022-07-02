9e4ba5bae71216358b0e6414be7d9bb9f6fc22b8
Rafael <rafa.ravg@gmail.com>
2022-07-02 09:20:06 -0700

    content query test

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,13 +1,15 @@
 <script setup>
 const { path } = useRoute();
-const { data } = await useAsyncData(`blog/${path}`, () => {
-  return queryContent().where({ _path: path }).findOne();
-});
+// const { data } = await useAsyncData(`blog/${path}`, () => {
+//   return queryContent().where({ _path: path }).findOne();
+// });
+
+const post = await queryContent().where({ _path: path }).findOne();
 
 useHead({
-  title: data.title,
+  title: post.title,
   meta: {
-    description: data.description,
+    description: post.description,
   },
 });
 </script>
@@ -18,7 +20,7 @@ useHead({
       class="hero-pattern container prose prose-pink max-w-none py-24 px-5 pt-48 prose-headings:text-secondary prose-h3:text-primary prose-p:text-lg prose-p:text-light prose-blockquote:bg-custom prose-blockquote:p-1 prose-strong:text-accent prose-code:text-light md:pt-24"
     >
       <figure>
-        <img :src="data.img" alt="" class="clay-effect mx-auto h-1/2 w-full rounded-2xl lg:w-1/2" />
+        <img :src="post.img" alt="" class="clay-effect mx-auto h-1/2 w-full rounded-2xl lg:w-1/2" />
       </figure>
       <ContentDoc class="container" />
       <!-- <ContentRenderer class="container" :value="data" /> -->
