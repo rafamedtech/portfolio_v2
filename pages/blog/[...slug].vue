70ac226a620aed5603bc4a0d2008de84825d8dea
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-12-04 15:02:45 -0800

    not found test 3

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -59,7 +59,15 @@ definePageMeta({
           <ContentRendererMarkdown :value="doc" />
         </template>
         <template v-slot:not-found>
-          <h1>post not found test</h1>
+          <div class="hero-pattern grid min-h-[75vh] place-items-center py-32">
+            <img src="@/assets/image/notfound.png" alt="" class="mx-auto h-[500px]" />
+            <h1 class="font-base mb-4 text-center text-3xl text-secondary">Page not found</h1>
+            <NuxtLink
+              to="/"
+              class="flex items-center rounded-lg border-0 bg-primary py-2 px-2 text-base text-black no-underline hover:bg-primary/75 focus:outline-none md:mt-0 md:inline-flex md:px-6"
+              >Return to Home</NuxtLink
+            >
+          </div>
         </template>
       </ContentDoc>
     </div>
